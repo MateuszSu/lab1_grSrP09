99328869d02ed899105bd31efc28e76bc508ba8d
Mats <259160@student.pwr.edu.pl>
2021-10-13 11:30:48 +0200

    podstawa programu

---
+++ src/org/mateusz/Main.java
@@ -0,0 +1,15 @@
+package org.mateusz;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(String.format("%2d ", i * j));
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
